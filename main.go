c809d6db3682ac4827009a7720107f4a17f2f9ad
Login Viktor <batazor111@gmail.com>
2015-12-06 02:44:35 +0300

    GraphQL on GO: 'Hello World'

--- main.go
+++ main.go
@@ -3,19 +3,39 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
+	"log"
+
+	"github.com/graphql-go/graphql"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		output, err := json.Marshal("Hello, wolrd")
+	// Schema
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
 
-		if err != nil {
-			fmt.Println("Something went wrong!")
+	// Query
+	query := `
+		{
+			hello
 		}
-
-		fmt.Fprintf(w, string(output))
-	})
-
-	http.ListenAndServe(":8080", nil)
+	`
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON) // {“data”:{“hello”:”world”}}
 }
