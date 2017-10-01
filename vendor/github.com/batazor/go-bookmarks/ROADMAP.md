80d2e7e0a413e76c5aafd76bad04e05e8ed5e812
Login Victor <batazor111@gmail.com>
2017-10-01 03:06:08 +0300

    Update depedncies

---
+++ vendor/github.com/batazor/go-bookmarks/ROADMAP.md
@@ -0,0 +1,7 @@
+# 0.3.0
+
+- /metrics (for Prometheus)
+- /health (for consul, k8s)
+- Docs API
+    - Postman
+    - godoc
---
+++ vendor/github.com/batazor/go-bookmarks/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/batazor/go-bookmarks/db"
+	"github.com/batazor/go-bookmarks/handlers/book"
+	"github.com/batazor/go-bookmarks/utils"
+	"github.com/go-chi/render"
+	"github.com/pressly/chi"
+	"github.com/pressly/chi/middleware"
+	"net/http"
+)
+
+var log = logrus.New()
+
+func init() {
+	// Logging =================================================================
+	// Setup the logger backend using Sirupsen/logrus and configure
+	// it to use a custom JSONFormatter. See the logrus docs for how to
+	// configure the backend at github.com/Sirupsen/logrus
+	log.Formatter = new(logrus.JSONFormatter)
+
+	// Connect to MongoDB
+	db.Connect()
+}
+
+func main() {
+
+	// Get configuration ======================================================
+	PORT := utils.Getenv("PORT", "4070")
+
+	// Routes ==================================================================
+	r := chi.NewRouter()
+
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", HelloWorld)
+	r.Mount("/book", book.Routes())
+
+	// start HTTP-server
+	log.Info("Run services on port " + PORT)
+	http.ListenAndServe(":"+PORT, r)
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!!!"))
+}
